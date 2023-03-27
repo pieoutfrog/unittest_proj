dce88cc4bb5b3184f44f21b23e6737149121795d
Alice <despero45@gmail.com>
2023-03-27 20:06:11 +1000

    work tests

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,10 +11,9 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
-        return default
-
-    return array[index]
+    if 0 <= index < len(array):
+        return array[index]
+    return default
 
 
 def my_slice(coll, start=0, end=None):
